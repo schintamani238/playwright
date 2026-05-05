c96c32eae1b5748b4f3d169caf880f399045662f
schintamani238 <smithachintamani238@gmail.com>
2026-05-05 06:49:03 -0700

    Fixing playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Run browsers in headed mode locally, headless in CI */
-    headless: false,
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
